f4dc4bfaa9bedf1b71d9bfc9fb9944cd991e677a
Guiioff <guilhermerivaldy288@hotmail.com>
2025-07-07 01:00:04 -0300

    feat: adiciona emojis na de tela inicial

--- src/components/WelcomeScreen.jsx
+++ src/components/WelcomeScreen.jsx
@@ -1,8 +1,29 @@
 import "./WelcomeScreen.css";
+import emojiAzul from "../assets/emoji-azul.png";
+import emojiAmarelo from "../assets/emoji-amarelo.png";
+import emojiLaranja from "../assets/emoji-laranja.png";
 
 export default function WelcomeScreen({ onStart }) {
   return (
     <div className="welcome-wrapper">
+      {/* Emoji azul (direita) */}
+      <img
+        src={emojiAzul}
+        alt="Emoji sorridente azul"
+        className="emoji emoji-azul"
+      />
+      {/* Emoji amarelo (esquerda, por baixo do laranja) */}
+      <img
+        src={emojiAmarelo}
+        alt="Emoji sorridente amarelo"
+        className="emoji emoji-amarelo"
+      />
+      {/* Emoji laranja (esquerda, mais acima) */}
+      <img
+        src={emojiLaranja}
+        alt="Emoji sorridente laranja"
+        className="emoji emoji-laranja"
+      />
       <div className="welcome-stack">
         <div className="welcome-banner-pair">
           <div className="welcome-banner-shadow universo-shadow"></div>
